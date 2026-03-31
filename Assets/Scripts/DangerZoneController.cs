5cd4a339c4777ac1f40153238d70d2378c576b37
zyesimm <140653206+zyesimm@users.noreply.github.com>
2026-03-31 12:13:20 +0300

    add danger zone countdown coroutine system

--- Assets/Scripts/DangerZoneController.cs
+++ Assets/Scripts/DangerZoneController.cs
@@ -1,23 +1,44 @@
 using UnityEngine;
-
+using System.Collections;
 public class DangerZoneController : MonoBehaviour
 {
     [SerializeField] private FlightExamManager examManager;
+    [SerializeField] private float missileDelay = 5f;
+
+    private Coroutine activeCountdown;
 
     private void OnTriggerEnter(Collider collision)
     {
-        Debug.Log("Bir şey zone'a girdi: " + collision.name);
-        if (collision.CompareTag("Player"))
+    if (collision.CompareTag("Player"))
+    {
+        Debug.Log("Player entered danger zone");
+        examManager.EnterDangerZone();
+
+        if (activeCountdown == null)
         {
-            examManager.EnterDangerZone();
+            activeCountdown = StartCoroutine(MissileCountdown());
         }
     }
-
+}
     private void OnTriggerExit(Collider collision)
     {
         if (collision.CompareTag("Player"))
         {
+            Debug.Log("Player exited danger zone");
             examManager.ExitDangerZone();
+
+            if (activeCountdown != null)
+            {
+                StopCoroutine(activeCountdown);
+                activeCountdown = null;
+            }
         }
+    }
+
+    private IEnumerator MissileCountdown()
+    {
+        yield return new WaitForSeconds(missileDelay);
+        Debug.Log("MISSILE SHOULD LAUNCH NIW");
+        activeCountdown = null;
     }
 }
